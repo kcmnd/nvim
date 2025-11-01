4f7234a0a99f63313bc4749f37a9cbd92a4d5383
kcmnd <khaled.chawa@gmail.com>
2025-11-01 13:36:45 -0400

    just cleaned up some code

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,27 +7,3 @@ return {
     vim.cmd("colorscheme miasma")
   end,
 }
-
--- return {
---   "theJian/nvim-moonwalk",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     vim.o.background = "light"           -- Ensure light mode
---     vim.cmd("colorscheme moonwalk")      -- Apply the Moonwalk theme
---
---     -- Make core UI elements transparent
---     vim.cmd [[
---       highlight Normal guibg=NONE ctermbg=NONE
---       highlight NormalNC guibg=NONE ctermbg=NONE
---       highlight EndOfBuffer guibg=NONE ctermbg=NONE
---       highlight VertSplit guibg=NONE ctermbg=NONE
---       highlight StatusLine guibg=NONE ctermbg=NONE
---       highlight StatusLineNC guibg=NONE ctermbg=NONE
---       highlight SignColumn guibg=NONE ctermbg=NONE
---       highlight LineNr guibg=NONE ctermbg=NONE
---       highlight FoldColumn guibg=NONE ctermbg=NONE
---       highlight CursorLine guibg=NONE ctermbg=NONE
---     ]]
---   end,
--- }
