cc20e61c4800e69c845ae86fe35a2aaf262a4c17
kcmnd <83512196+kcmnd@users.noreply.github.com>
2026-01-31 17:59:21 -0500

    update to newer neovim version and newer treesitter
    Signed-off-by: kcmnd <83512196+kcmnd@users.noreply.github.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,54 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",  -- Explicitly use main branch
+  lazy = false,
   build = ":TSUpdate",
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
+  config = function()
+    local ts = require("nvim-treesitter")
 
-    configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-      auto_install = { enable = true },
+    -- Install parsers you want
+    local parsers = {
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "python",
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      "bash",
+      "json",
+      "markdown",
+    }
+
+    -- Install parsers (async, won't block startup)
+    ts.install(parsers)
+
+    -- Enable highlighting via autocmd (new API)
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = parsers,
+      callback = function()
+        -- Only start if parser is available
+        local lang = vim.treesitter.language.get_lang(vim.bo.filetype) or vim.bo.filetype
+        if pcall(vim.treesitter.language.inspect, lang) then
+          vim.treesitter.start()
+        end
+      end,
     })
-  end
+
+    -- Enable indentation (experimental)
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = parsers,
+      callback = function()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+
+    -- Optional: Enable folding
+    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo.foldmethod = 'expr'
+    vim.opt.foldlevel = 99  -- Don't fold by default
+  end,
 }
