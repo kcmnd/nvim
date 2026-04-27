03f54e8b15173a7113989673fd33f6d42f295cf2
kcmnd <83512196+kcmnd@users.noreply.github.com>
2026-04-26 23:02:28 -0400

    autocomplete now works with languages other than just python
    Signed-off-by: kcmnd <83512196+kcmnd@users.noreply.github.com>

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -19,8 +19,7 @@ return {
         snippet = {
           -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            require('luasnip').lsp_expand(args.body)
           end,
         },
         window = {
